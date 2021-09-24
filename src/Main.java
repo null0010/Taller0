a2715bc2f8642a732309be2853132afdd02299e5
NullCoder <90267124+null0010@users.noreply.github.com>
2021-09-24 10:28:45 +0900

    Update Main.java
    Se agregaron las matrices y listas que serán necesarias a lo largo de la aplicación.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,35 @@
 public class Main {
     public static void main(String[] args) {
+        String[] listaNombresClientes = new String[999];
+        String[] listaApellidosClientes = new String[999];
+        String[] listaRutsClientes = new String[999];
+        String[] listaContraseñasClientes = new String[999];
+        String[] listaEstadoPaseMovilidadClientes = new String[999];
+
+        String[] listaNombresPeliculasEntradasCompradas = new String[999];
+        int[] listaHorariosEntradasCompradas = new int[999];
+        String[] listaAsientosEntradasCompradas = new String[999];
+
+        String[] listaFilasSalas = {"A",
+                "B",
+                "C",
+                "D",
+                "E",
+                "F",
+                "G",
+                "H",
+                "I",
+                "J"};
+        int[][] matrizSalaCine1 = generarSalaCine();
+        int[][] matrizSalaCine2 = generarSalaCine();
+        int[][] matrizSalaCine3 = generarSalaCine();
+        int[][] matrizHorariosFuncionesSalas = new int[999][3];
+
+        String[] listaNombresPeliculas = new String[999];
+        String[] listaTipoPeliculas = new String[999];
+        int[] listaRecaudacionesPeliculas = new int[999];
+        int[] listaRecaudacionesMañanaPeliculas = new int[999];
+        int[] listaRecaudacionesTardePeliculas = new int[999];
+        int[] listaRecaudacionesDuranteDiaPeliculas = new int[999];
     }
 }
