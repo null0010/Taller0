c999fd3179540c2fc2c62e313a9f21d9a918a2b2
ynowmy <francisco.bordones@alumnos.ucn.cl>
2021-09-24 15:49:05 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -42,8 +42,8 @@ public class Main {
         for (int f = 0; f < NUMERO_FILAS - 6; f++) {
             for (int c = 5; c < NUMERO_COLUMNAS - 5; c++) {
                 matrizSala[f][c] = -1;
-            System.out.println("0000000000");
-            System.out.println("0000000000");
+            
+            
             }
         }
 
